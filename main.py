b8bc00689c0c755d1e463ad84e0b0a3d33fbcb64
deltea <thcheetah777@gmail.com>
2025-10-11 18:07:47 -0700

    we got webcam output!

---
+++ main.py
@@ -0,0 +1,17 @@
+import cv2
+
+cam = cv2.VideoCapture(0, cv2.CAP_V4L2)
+
+cv2.namedWindow("Imagetest", cv2.WND_PROP_FULLSCREEN)
+cv2.setWindowProperty("Imagetest", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+while True:
+	ret, image = cam.read()
+	cv2.imshow("Imagetest", image)
+	k = cv2.waitKey(1)
+	if k != -1:
+		break
+
+cv2.imwrite("/home/leo/test.jpg", image)
+cam.release()
+cv2.destroyAllWindows()
